0b286fe4657a509d6691625d5163c67022e00e66
Ferit Cem Kahraman <frtcm11@gmail.com>
2023-01-26 18:09:02 +0300

    tasarim degisikligi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("daisyui")],
 };
