790b4937fa9819c28d8ceecbb905664d29597e68
Ferit Cem Kahraman <frtcm11@gmail.com>
2023-01-29 23:35:33 +0300

    sayfa düzenlemeleri

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,39 +1,43 @@
 <template>
-  <nav class="bg-pink-100 px-2 sm:px-4 py-2.5">
-    <div class="container flex flex-wrap items-center justify-between mx-auto">
-      <a href="/" class="flex items-center">
-        <img src="/img/logo-500.png" class="h-14 mr-3 sm:h-20" alt="Kısmetse Olur" />
-      </a>
-      <div class="flex md:order-2">
-        <button type="button" class="text-white bg-pink-600 hover:bg-pink-500 transition duration-600 font-medium rounded-lg text-md px-8 py-2.5 text-center mr-3 md:mr-0">Başvur</button>
-        <button
-          data-collapse-toggle="navbar-cta"
-          type="button"
-          class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-          aria-controls="navbar-cta"
-          aria-expanded="false"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
-          </svg>
-        </button>
-      </div>
-      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-        <ul class="flex flex-col p-4 mt-4 md:flex-row md:space-x-24 md:mt-0 md:text-md md:font-medium md:border-0 md:bg-pink-100">
+  <div class="navbar bg-pink-100">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-secondary lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-500 rounded-box w-52">
           <li>
-            <router-link to="/" class="block py-2 pl-3 pr-4 text-black md:text-black text-lg md:p-0 focus:text-red-500 hover:scale-125 duration-200" aria-current="page">Anasayfa</router-link>
+            <router-link to="/" class="block py-2 pl-3 pr-4 text-black md:text-black text-lg font-bold md:p-0 focus:text-red-500 duration-200" aria-current="page">Anasayfa</router-link>
           </li>
           <li>
-            <router-link to="/duyurular" class="block py-2 pl-3 pr-4 text-black md:text-black text-lg md:p-0 focus:text-red-500 hover:scale-125 duration-200" aria-current="page">Duyurular</router-link>
+            <router-link to="/duyurular" class="block py-2 pl-3 pr-4 text-black md:text-black text-lg font-bold md:p-0 focus:text-red-500 duration-200" aria-current="page">Duyurular</router-link>
           </li>
           <li>
-            <router-link to="/iletisim" class="block py-2 pl-3 pr-4 text-black md:text-black text-lg md:p-0 focus:text-red-500 hover:scale-125 duration-200" aria-current="page">İletişim</router-link>
+            <router-link to="/iletisim" class="block py-2 pl-3 pr-4 text-black md:text-black text-lg font-bold md:p-0 focus:text-red-500 duration-200" aria-current="page">İletişim</router-link>
           </li>
         </ul>
       </div>
+      <a href="/" class="flex items-center sm:ml-12">
+        <img src="/img/logo-500.png" class="h-14 sm:h-20" alt="Kısmetse Olur" />
+      </a>
     </div>
-  </nav>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal lg:pl-12 lg:gap-20">
+        <li>
+          <router-link to="/" class="block py-2 pl-3 pr-4 text-black md:text-black text-lg font-bold md:p-0 focus:text-red-500 hover:scale-125 hover:bg-transparent focus:bg-pink-100 duration-200" aria-current="page">Anasayfa</router-link>
+        </li>
+        <li>
+          <router-link to="/duyurular" class="block py-2 pl-3 pr-4 text-black md:text-black text-lg font-bold md:p-0 focus:text-red-500 hover:scale-125 hover:bg-transparent focus:bg-pink-100 duration-200" aria-current="page">Duyurular</router-link>
+        </li>
+        <li>
+          <router-link to="/iletisim" class="block py-2 pl-3 pr-4 text-black md:text-black text-lg font-bold md:p-0 focus:text-red-500 hover:scale-125 hover:bg-transparent focus:bg-pink-100 duration-200" aria-current="page">İletişim</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="navbar-end lg:pr-48">
+      <button type="button" class="text-white bg-pink-600 hover:bg-pink-500 transition duration-600 font-medium rounded-lg text-md px-8 py-2.5 text-center sm:ml-3 md:mr-0">Başvur</button>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
         <div class="card-actions">
           <button class="btn btn-primary px-32 mb-2 text-lg bg-pink-600 hover:bg-pink-500 border-none normal-case">Başvur</button>
         </div>
-        <div class="card-actions">
+        <div class="card-actions mb-2">
           <router-link :to="{ path: '/aydinlatma-metni' }" target="_blank"><button class="btn btn-primary px-4 border-l text-lg bg-pink-600 hover:bg-pink-500 border-none normal-case">Yarışmacı Adayı Aydınlatma Metni</button></router-link>
         </div>
       </div>
