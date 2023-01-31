c816e3823d3b4a651718736b09caf0dd4f38f802
Ferit Cem Kahraman <frtcm11@gmail.com>
2023-01-31 13:03:50 +0300

    basic settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
